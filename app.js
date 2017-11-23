a93f85c9b418a8314b4db5e192df1a8231d343f5
alirezaekt <tiduspattern@gmail.com>
2017-11-23 13:21:28 +0330

    some fixes

--- app.js
+++ app.js
@@ -4,13 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var index = require('./routes/index');
 var users = require('./routes/users');
 var voip = require('./routes/voip');
-
-
-
 
 var app = express();
 
@@ -20,21 +16,18 @@ app.set('views', path.join(__dirname, 'public/views'));
 app.engine('html',require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use("/",express.static(__dirname+'/public'));
-
 app.use('/', index);
 app.use('/users', users);
 app.use('/voip', voip);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Not Found-__-');
   err.status = 404;
   next(err);
 });

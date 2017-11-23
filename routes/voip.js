a8cd42c77f7f9442601d33e8ed15d931254685c6
alirezaekt <tiduspattern@gmail.com>
2017-11-23 11:25:10 +0330

    some fixes

--- routes/voip.js
+++ routes/voip.js
@@ -8,12 +8,12 @@ router.get('/', function(req, res) {
 
 router.post('/voice', function(req, res) {
     var username = req.body.userId ;
-    res.send(username + "  : kir :)))");
+    res.send({gateway:{url:"http://192.168.137.93:8080/stream"}});
 });
+
 router.post('/video', function(req, res) {
     var userId = req.body.userId ;
     console.log(userId);
-
     res.send({gateway:{url:"http://192.168.137.93:8080/stream"}});
 });
 module.exports = router;

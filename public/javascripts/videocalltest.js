4e743e4eb56433622a11e6117980194cd674f634
maverick26 <mohamad.ahmadi26@gmail.com>
2017-12-12 12:08:41 +0330

    videocalltest fixed

--- public/javascripts/videocalltest.js
+++ public/javascripts/videocalltest.js
@@ -44,9 +44,9 @@
 //
 var server = null;
 if(window.location.protocol === 'http:')
-    server = "http://192.168.137.215:8088/janus";
+    server = "http://192.168.137.59:8088/janus";
 else
-    server = "http://192.168.137.215:8088/janus";
+    server = "http://192.168.137.59:8088/janus";
 
 //CM by Me
 //server = "/janus";

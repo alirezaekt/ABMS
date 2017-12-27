28eb2c50916cfafc1313034f3eeb0e05bfb5e0c5
alirezaekt <tiduspattern@gmail.com>
2017-12-27 11:47:35 +0330

    added new elements

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-
 /* GET home page. */
 router.get('/', function(req, res) {
     res.render('index.html');
@@ -14,10 +13,47 @@ router.post('/admin', function(req, res) {
 });
 module.exports = router;
 
-
 //--- serial funcrions
-
 router.get('/serialp',function (req,res) {
+    var buffer = new Buffer(3);
+    buffer[0] = 0x05;
+    buffer[1] = 0x00;
+    buffer[2] = 0x0F;
+
+    var SerialPort = require('serialport');
+    var port = new SerialPort('/dev/serial0',{
+            baudRate: 115200,
+            databits: 8,
+            parity: 'none',
+            autoOpen :false
+        }
+    );
+    port.open(function (err) {
+        if (err) {
+            return console.log('Error opening port: ', err.message);
+        }
+        console.log('hi');
+    });
+    port.on('data',function(data){
+        console.log('data :  ',data) ;
+    });
+
+    port.write(buffer, function(err) {
+        if (err) {
+            return console.log('Error on write: ', err.message);
+        }
+        console.log('message written');
+    });
+
+// Open errors will be emitted as an error event
+    port.on('error', function(err) {
+        console.log('Error: ', err.message);
+    })
+    console.log("serialdone")
+});
+
+
+router.get('/serialp2',function (req,res) {
     var buffer = new Buffer(3);
     buffer[0] = 0x05;
     buffer[1] = 0x80;
